1c7857fb703af578ef7c59a90e1b3ef29572bedc
roomten <jcm098@outlook.com>
2020-11-27 13:45:48 +0000

    added clapper button!

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">{{ count }} 👏 claps</button>'
+})
+
+
 let app = new Vue ({
   el: '#app',
   data: {
